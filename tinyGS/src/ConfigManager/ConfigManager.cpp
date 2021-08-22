2a754982c538d18b973fa0d862bf167ff0cd7516
luisgcu <angelgcu@gmail.com>
2021-08-22 13:33:51 -0400

    add LuisGCU gawteway pin def

--- tinyGS/src/ConfigManager/ConfigManager.cpp
+++ tinyGS/src/ConfigManager/ConfigManager.cpp
@@ -47,6 +47,7 @@ ConfigManager::ConfigManager()
                                                                                                                                         {0x3c, 21, 22, 16, 38, 22, 1, 18, 26, 33, 0, 14, 19, 27, 5, 0.0f, "T-BEAM V1.0 + OLED"},                                         // @fafu
                                                                                                                                         {0x3c, 21, 22, 16, 0, 2, 0, 5, 0, 34, 32, 14, 19, 27, 18, 1.6f, "433Mhz FOSSA 1W Ground Station"},                               // @jgromes
                                                                                                                                         {0x3c, 21, 22, 16, 0, 2, 0, 5, 0, 34, 32, 14, 19, 27, 18, 1.6f, "868-915Mhz FOSSA 1W Ground Station"},                           // @jgromes
+                                                                                                                                        {0x3c, 21, 22, 16, 0, 27, 1, 5, 15, 13, 11, 14, 19, 23, 18, 1.6f, "433MHZ RFM96  LUISGCU"},                                       // @luisgcu
                                                                                                                                     })
 {
   server.on(ROOT_URL, [this] { handleRoot(); });
